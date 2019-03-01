20c58a4998b27ebba1683be5dbf2618717005ea9
miltonl <mlau@tavnetworks.com>
2019-03-01 14:53:24 -0800

    Added identifier to pole

--- Pole.py
+++ Pole.py
@@ -3,10 +3,11 @@ class Pole(object):
     lat = float(0)
     lon = float(0)
 
-    def __init__(self, lat, lon):
+    def __init__(self, tag, lat, lon):
+        self.id = tag
         self.lat = lat
         self.lon = lon
 
-def make_pole(lat, lon):
-    pole = Pole(lat, lon)
+def make_pole(tag, lat, lon):
+    pole = Pole(tag, lat, lon)
     return pole
